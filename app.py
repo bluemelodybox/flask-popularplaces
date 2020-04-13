f92c20a6770078333af97ebc459079da3d678e2c
Wesley Chew <wesleychewjj@gmail.com>
2020-04-13 17:06:52 +0800

    updates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify
+import populartimes
+import os
 
 app = Flask(__name__)
 
@@ -31,7 +33,15 @@ def respond():
 # A welcome message to test our server
 @app.route("/")
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    API_KEY = os.environ["GOOGLE_API_KEY"]
+    places = [
+        "ChIJk_idN3oU2jEReqhHxnv3lgI",  # Chongpang market
+        "ChIJjyjjwCAX2jERxYHvTxAw4X0",  # Bishan park
+        "ChIJeRqAraYX2jERQpyIAXSU1SU",  # Nex shopping mall
+    ]
+    res = [populartimes.get_id(API_KEY, place) for place in places]
+
+    return jsonify(res)
 
 
 if __name__ == "__main__":
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export FLASK_APP=app.py
-export FLASK_ENV=deployment
+# export FLASK_ENV=deployment
+export FLASK_ENV=development
 flask run

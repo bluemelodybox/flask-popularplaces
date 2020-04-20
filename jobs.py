43a56773af3f1fe2fd84f844b24daddd83743369
Wesley Chew <wesleychewjj@gmail.com>
2020-04-20 11:08:23 +0800

    tet

--- jobs.py
+++ jobs.py
@@ -40,6 +40,7 @@ def timed_job():
         "Mrt Station": [
             "ChIJCZRupukR2jERCglyJsNXaHE",  # CCK Station
             "ChIJf9yot4cX2jERNWnz7pUJ0lM",  # AMK Station
+            "ChIJNTlhccg92jERBftThrByZK0",  # Pasir ris park
         ],
     }
 
